b9e96fb08d64564ac6f40907082ec9a9bbf4441b
harsukh.kaur <hkaur.toor70@gmail.com>
2017-07-02 21:07:38 +0100

    committed HelloDemo project

---
+++ Demo/src/Demo/HelloWorld.java
@@ -0,0 +1,27 @@
+package Demo;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class HelloWorld {
+	
+	WebDriver driver;
+	@Test
+	
+	public void open_url()
+	{
+	
+	driver = new FirefoxDriver();
+	
+	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	driver.manage().window().maximize();
+	driver.get("https://www.google.co.uk/");
+			
+	driver.getTitle();
+System.out.println(driver.getTitle());
+	
+	}
+}
